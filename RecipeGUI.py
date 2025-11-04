73c49ec24de0386bf4f5d05c7626b1a90e63c986
idaugherty <isabelladaugherty5@gmail.com>
2025-11-04 10:34:54 -0500

    Replaced input boxes with text areas
    For the ingredients and instructions, I replaced their input boxes with text widgets

--- RecipeGUI.py
+++ RecipeGUI.py
@@ -8,23 +8,23 @@ root.title("Recipe Book  App")
 #setting screen size
 root.geometry("350x250")
 
-#creating recipe label and input box
+#creating recipe label and text area(widget)
 recipeLabel = Label(root, text = "Enter the recipe name")
-recipeLabel.grid(row=0, column=0)
+recipeLabel.grid(padx=10, pady=10,row=0, column=0)
 recipeEntryBox = Entry(root, width=20)
-recipeEntryBox.grid(row=0, column=2)
+recipeEntryBox.grid(padx=10, pady=10,row=0, column=2)
 
-#creating ingredients label and input box
+#creating ingredients label and text area(widget)
 ingredientsLabel = Label(root, text = "Enter the ingredients ")
-ingredientsLabel.grid(row=2, column=0)
-ingredientsEntryBox = Entry(root, width=20)
-ingredientsEntryBox.grid(row=2, column=2)
+ingredientsLabel.grid(padx=10, pady=10, row=2, column=0)
+ingredientsTextWidget = tkinter.Text(root, height=5, width = 20)
+ingredientsTextWidget.grid(padx=10, pady=10, row=2, column=2)
 
-#creating instructions label and input box
+#creating instructions label and text area(widget)
 instructionsLabel = Label(root, text = "Enter the instructions ")
-instructionsLabel.grid(row=4, column=0)
-instructionsEntryBox = Entry(root, width=20)
-instructionsEntryBox.grid(row=4, column=2)
+instructionsLabel.grid(padx=10, pady=10, row=4, column=0)
+instructionsTextWidget = tkinter.Text(root, height=5, width = 20)
+instructionsTextWidget.grid(padx=10, pady=10, row=4, column=2)
 
 
 #run the event loop
